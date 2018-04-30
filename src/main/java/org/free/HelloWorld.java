3c62d188b3ee13d6cdfb746881b2abdac87d871a
Fethi Lamraoui <fethi.lamraoui@desjardins.com>
2018-04-30 15:47:41 -0400

    Resolving conflicts

--- src/main/java/org/free/HelloWorld.java
+++ src/main/java/org/free/HelloWorld.java
@@ -3,11 +3,8 @@ package org.free;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
-<<<<<<< HEAD
 		System.out.println("first branch first commit!!");
-=======
 		System.out.println("second branch first commit!!");
 		System.out.println("second branch second commit!!");
->>>>>>> branch2
 	}
 }
